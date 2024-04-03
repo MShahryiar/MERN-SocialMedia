09f62dda56f538488c1ec3c965e51a89cccb781a
MShahryiar <muhammadshahryiar@yahoo.com>
2024-04-03 13:59:24 +0500

    First Commit

---
+++ Client/Client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-200 bg-green-200">Social Media </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+
+# Social Media Website
+
+## A Full Stack Social Media website developed using MERN Stack.
